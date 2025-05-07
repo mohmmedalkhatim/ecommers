caf47e0831cbc7a01187108783482d73ff85e8f5
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-05-07 12:53:10 +0200

    working on the style of the header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import './index.css';
+import Header from './components/Header';
+
 function App() {
+  console.log("hello")
   return (
     <>
+      <Header />
       <Outlet />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,40 @@
-import React, { ButtonHTMLAttributes } from "react"
+import { AllHTMLAttributes } from "react"
+import Logo from "../Logo"
+import { Link } from "react-router-dom"
+import Input from "../Input"
+import { TbSearch } from "react-icons/tb"
 
 
 let links = [
-    {name:"",url:""},
-    {name:"",url:""},
-    {name:"",url:""},
-    {name:"",url:""},
-    {name:"",url:""},
+    { name: "Home", url: "" },
+    { name: "products", url: "" },
+    { name: "collation", url: "" },
+    { name: "", url: "" },
+    { name: "", url: "" },
 ]
 type Header_Props = {
 
-} & ButtonHTMLAttributes<HTMLDivElement> 
+} & AllHTMLAttributes<HTMLDivElement>
 
-function Header(props:Header_Props) {
-    
+function Header(props: Header_Props) {
     return (
-        <header {...props}></header>
+        <header className="flex w-screen py-4" {...props}>
+            <div className="container flex mx-32 flex-col gap-4
+            ">
+                <div className="flex justify-between">
+                    <Logo />
+                    <Input icon={<TbSearch size={"1rem"} />} />
+                </div>
+                <nav className="flex list-none gap-4">
+                    {links.map((item, i) => (
+                        <Link to={item.url} key={i}>
+                            <li>{item.name}</li>
+                        </Link>
+                    ))}
+                </nav>
+
+            </div>
+        </header>
     )
 }
 export default Header
