7fb43efb9b52070c124eee41870ee600b7152e1a
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-05-10 17:02:19 +0200

    adding logo and improving the header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,8 +9,8 @@ let links = [
     { name: "Home", url: "" },
     { name: "products", url: "" },
     { name: "collation", url: "" },
-    { name: "", url: "" },
-    { name: "", url: "" },
+    { name: "about", url: "" },
+    { name: "my_account", url: "" },
 ]
 type Header_Props = {
 
@@ -19,11 +19,11 @@ type Header_Props = {
 function Header(props: Header_Props) {
     return (
         <header className="flex w-screen py-4" {...props}>
-            <div className="container flex mx-32 flex-col gap-4
+            <div className="container flex px-48 py-4 flex-col gap-4
             ">
                 <div className="flex justify-between">
                     <Logo />
-                    <Input icon={<TbSearch size={"1rem"} />} />
+                    <Input icon={<TbSearch size={"1rem"} />} className="w-[20rem]" placeholder="what are you looking for...." />
                 </div>
                 <nav className="flex list-none gap-4">
                     {links.map((item, i) => (
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import { ReactElement, useState } from 'react';
 
 type InputProps = {
   label?: string;
@@ -10,6 +9,7 @@ type InputProps = {
   error?: string;
   required?: boolean;
   className?: string;
+  icon?: ReactElement;
 };
 
 export default function Input({
@@ -20,30 +20,42 @@ export default function Input({
   onChange,
   error,
   required = false,
+  icon,
   className = '',
 }: InputProps) {
   const [touched, setTouched] = useState(false);
+  const hasError = error && touched;
 
   return (
-    <div className={`flex flex-col gap-1 rounded-lg ${className}`}>
+    <div className={`flex flex-col gap-1 ${className}`}>
       {label && (
         <label className="text-sm font-medium text-white">
           {label} {required && <span className="text-red-500">*</span>}
         </label>
       )}
-      <input
-        type={type}
-        value={value}
-        placeholder={placeholder}
-        onChange={(e) => onChange?.(e.currentTarget.value)}
-        onBlur={() => setTouched(true)}
-        className={`border-[1px] border-[#e2e2e220] rounded-md p-2 outline-none bg-transparent text-white focus:ring-1 focus:ring-blue-100 transition-all ${
-          error && touched ? 'border-red-500' : ''
-        }`}
-      />
-      {error && touched && (
-        <span className="text-red-500 text-xs">{error}</span>
-      )}
+
+      <div
+        className={`group flex items-center border rounded-md transition-all
+          ${hasError ? 'border-red-500' : 'border-stone-50'} 
+          focus-within:ring focus-within:ring-[#e2e2e220]`}
+      >
+        <input
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          onChange={(e) => onChange?.(e.target.value)}
+          onBlur={() => setTouched(true)}
+          className="grow outline-none border-none text-xs rounded-none  bg-transparent placeholder:text-[#ffffff90]"
+        />
+        <div
+          className={`rounded-r-sm pb-1 pt-[4px] pr-2  transition-colors
+            ${hasError ? 'text-red-500' : 'group-focus-within:text-white'}`}
+        >
+          {icon}
+        </div>
+      </div>
+
+      {hasError && <span className="text-red-500 text-xs">{error}</span>}
     </div>
   );
 }
