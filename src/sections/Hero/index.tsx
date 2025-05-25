be3054bbfd958f8003a1fa36305e56ef634b35e5
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-05-25 17:48:55 +0200

    removing some files

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -2,22 +2,22 @@ import Button from '../../components/Button';
 
 function Hero () {
   return (
-    <section className='content'>
-      <div className='hero_content'>
+    <section className='content h-[35rem]'>
+      <div className='hero_content h-full py-8'>
         <div className=''>
-          <div className='hero_call_to_action'>
+          <div className='hero_call_to_action pt-[5rem] h-full'>
             <h1>welcome</h1>
-            <p>
+            <p className='w-[27rem]'>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Distinctio veniam id consequatur tempora recusandae, similique
               odit.
             </p>
             <div>
-              <Button children='hello' />
+              <Button children='know more' />
             </div>
           </div>
         </div>
-        <div className='background border hidden md:block'></div>
+        <div className='background border h-full hidden md:block'></div>
       </div>
     </section>
   );
