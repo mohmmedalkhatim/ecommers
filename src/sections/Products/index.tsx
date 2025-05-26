035c91e3567a70bccb6ee95c2a76c5c0087ce4b5
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-05-26 05:57:54 +0200

    building the products page to contain a filter

--- src/sections/Products/index.tsx
+++ src/sections/Products/index.tsx
@@ -1,10 +1,22 @@
-function Products() {
-  return (
-    <section className="content">
-        <div>
+import { TbSearch } from 'react-icons/tb';
+import Input from '../../components/Input';
 
+let list = [{ name: 'keyboard' }];
+
+function Products () {
+  return (
+    <main>
+      <section className='content products'>
+        <div className='filter'>
+          <form action=''>
+            <Input icon={<TbSearch />} />
+          </form>
         </div>
-    </section>
-  )
+        <div className='col-span-3'>
+          
+        </div>
+      </section>
+    </main>
+  );
 }
-export default Products
+export default Products;
