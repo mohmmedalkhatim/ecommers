0bd0f0429421028502a48bb0c5d5af295e14395e
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-02 15:55:30 +0200

    adding links to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,21 +6,21 @@ import MyLink from './Link';
 import { useAside } from '../../context/Aside';
 
 let links = [
-  { name: 'Home', url: '' },
+  { name: 'Home', url: '/' },
   { name: 'products', url: '/products' },
-  { name: 'collation', url: '' },
-  { name: 'about', url: '' },
+  { name: 'collation', url: '/collations' },
+  { name: 'about', url: '/about' },
 ];
 let info = [
   { name: 'my_account', url: '/auth', icon: <TbUser size={'1.5rem'} /> },
-  { name: 'cart', url: '', icon: <TbBasket size={'1.5rem'} /> },
+  { name: 'cart', url: '/cart', icon: <TbBasket size={'1.5rem'} /> },
 ];
 type Header_Props = {} & AllHTMLAttributes<HTMLDivElement>;
 
 function Header (props: Header_Props) {
   let openAsdie = useAside(state => state.toggle);
   return (
-    <header className={'border-b py-4 text-sm  w-full bg-white '} {...props}>
+    <header className={'border-b py-4 text-sm  w-full  bg-white '} {...props}>
       <div className='md:flex hidden content w-full  flex-col gap-4 '>
         <div className='flex justify-between'>
           <Logo />
