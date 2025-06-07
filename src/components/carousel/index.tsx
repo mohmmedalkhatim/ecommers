7557f0f1accdd778dd875cecaeb70c14af2c6829
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-07 06:41:07 +0200

    fixing bugs on the carousel

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -10,34 +10,32 @@ function Carousel () {
     ref.current?.scrollTo(val, 0);
   }, [active]);
   return (
-    <section>
-      <ul className='carousel list-none relative transition-all' ref={ref}>
+    <section className='content  relative'>
+      <ul className='carousel px-[2rem] list-none border  rounded' ref={ref}>
         <div
-          className='fixed left-0 content flex items-center  h-[20rem]'
+          className='absolute lg:-left-[4rem] left-0 bg-white  to-[#e5e7eb] ml-8 sm:ml-12 md:ml-40 xl:ml-48  border flex items-center  h-[20rem]'
           onClick={() => {
             let val = active * 16 * 20 - 16;
-            let width = ref.current?.scrollWidth;
-            if (width) {
-              console.log(width);
-              console.log(val);
-              width < val ? setActive(active - 1) : {};
+            let width = ref.current?.clientWidth;
+            let scroll = ref.current?.scrollWidth;
+            if (width && scroll) {
+              scroll - width < val ? setActive(active - 1) : setActive(0);
             }
           }}
         >
           <BiChevronLeft size={'3rem'} />
         </div>
         {list?.map((_, i) => (
-          <li id={`re${i}`} key={i} onClick={() => setActive(i)}></li>
+          <li className='border' key={i} onClick={() => setActive(i)}></li>
         ))}
         <div
-          className='fixed right-0 content flex items-center  h-[20rem]'
+          className='absolute lg:-right-[4rem] right-0 bg-white mr-8 sm:mr-12 md:mr-40 xl:mr-48 border flex items-center  h-[20rem]'
           onClick={() => {
-            let val = active * 16 * 20 - 16;
-            let width = ref.current?.scrollWidth;
-            if (width) {
-              console.log(width);
-              console.log(val);
-              width > val ? setActive(active + 1) : setActive(0);
+            let val = active * 16 * 20 + 16;
+            let width = ref.current?.clientWidth;
+            let scroll = ref.current?.scrollWidth;
+            if (width && scroll) {
+              scroll - width > val ? setActive(active + 1) : setActive(0);
             }
           }}
         >
