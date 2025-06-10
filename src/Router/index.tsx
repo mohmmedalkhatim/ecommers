5094c7b10eac86f532d89a5ec7e9fb78d6534820
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-10 20:08:58 +0200

    adding pages to the router

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -5,15 +5,27 @@ import {
 } from 'react-router-dom';
 import App from '../App';
 import Home from '../screens/Home';
-import Products from '../sections/Products';
-import ProductPage from '../sections/Products/product_page';
+import Products from '../screens/Products';
+import ProductPage from '../screens/Products/product_page';
+import Auth from '../screens/Auth';
+import Signin from '../screens/Auth/Signin';
+import Signup from '../screens/Auth/Signup';
+import Cart from '../screens/Cart';
+import Collations from '../screens/collations';
+import About from '../screens/About';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Home />} index />
+      <Route path="/sign_in" element={<Signin />}/>
+      <Route path="/sign_up" element={<Signup />}/> 
+      <Route path="/auth" element={<Auth />}/> 
       <Route path="/products" element={<Products />} />
       <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/collations" element={<Collations/>} />
+      <Route path="/about" element={<About/>} />
     </Route>
   )
 );
