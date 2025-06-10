d73de2c2054f60323fed4a93cedcbf63a674884a
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-10 20:07:08 +0200

    adding the useCart functionality

--- src/components/product_card/index.tsx
+++ src/components/product_card/index.tsx
@@ -1,15 +1,22 @@
-import { Product } from '../../context/cart';
+import { useCart } from '../../context/cart';
+import { Product } from '../../context/Product';
 import Button from '../Button';
 
-function ProductCard({ image, name, price }: Product) {
+function ProductCard ({ image, name, price }: Product) {
+  let add_Product = useCart(state => state.add_Product);
   return (
-    <li className="border rounded relative list-none min-w-[16rem] max-w-[18rem]">
-      <img src={image} alt="" className="border rounded-t" height={300} />
-      <div className="flex gap-4 items-center justify-between p-4">
-        <div>{name}</div>
-        <div className="flex items-center gap-4">
+    <li className='border rounded mx-auto relative list-none min-w-[16rem] max-w-[18rem]'>
+      <img src={image} alt='' className='border rounded-t' height={300} />
+      <div className='flex gap-4 items-center justify-between p-4'>
+        <div className=''>{name}</div>
+        <div className='flex items-center gap-4'>
           <div>{price}</div>
-          <Button children="cart" />
+          <Button
+            children='cart'
+            onClick={() => {
+              add_Product({ image, name, price });
+            }}
+          />
         </div>
       </div>
     </li>
