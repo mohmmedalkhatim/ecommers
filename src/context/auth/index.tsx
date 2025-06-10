d6062e98807d09f587decac4e466609be43e0940
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-10 20:08:21 +0200

    working on the auth logic

--- src/context/auth/index.tsx
+++ src/context/auth/index.tsx
@@ -1,46 +1,61 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 import Pocketbase from 'pocketbase';
+import {
+  Link,
+  redirect,
+  redirectDocument,
+  resolvePath,
+} from 'react-router-dom';
 
 let pb = new Pocketbase('http://127.0.0.1:8090');
 
 class Error {
-    isError: boolean = false;
-    details: string = ""
-    constructor(details: string) {
-        this.isError = true;
-        this.details = details
-    }
+  isError: boolean = false;
+  details: string = '';
+  constructor (details: string) {
+    this.isError = true;
+    this.details = details;
+  }
 }
+
+export type User = {
+  email: string;
+  emailVisibility: boolean;
+  name: string;
+  orders: string[];
+  password: string;
+  passwordConfirm: string;
+};
 
 interface Auth_Logic {
-    loading: boolean,
-    Error?: Error
-    signup: () => Promise<void>,
-    login: () => void,
-    delete: () => void,
-    retrieve: () => void,
+  loading: boolean;
+  login: boolean;
+  Error?: Error;
+  signup: (data: User) => Promise<void>;
+  logout: () => Promise<void>;
+  signin: (email: string, password: string) => Promise<void>;
+  delete: () => void;
+  retrieve: () => void;
 }
 
-let usePosts = create<Auth_Logic>(set => ({
-    loading: false,
-    signup: async () => {
-        set({ loading: true })
-        const data = {
-            "title": "test",
-            "content": "test"
-        };
-        
-        let record;
-        pb.collection('posts').create(data).then(res => {
-            record = res
-        }).catch(error => {
-            set({ Error: new Error(error) })
-        }).finally(() =>
-            set({ loading: false })
-        )
-    },
-    login:()=>{},
-    update: () => { },
-    delete: () => { },
-    retrieve: () => { },
-}))
+export let useAuth = create<Auth_Logic>(set => ({
+  loading: false,
+  login: false,
+  signup: async (data: User) => {
+    const record = await pb.collection('users').create(data);
+  },
+  signin: async (email, password) => {
+    await pb.collection('users').authWithPassword(email, password);
+    console.log(pb.authStore.record);
+    if (pb.authStore.isValid) {
+      set({ login: true });
+    }
+  },
+  logout: async ()=>{
+    pb.authStore.clear()
+    set({login:false})
+  },
+  update: () => {},
+  delete: () => {},
+  retrieve: () => {},
+}));
