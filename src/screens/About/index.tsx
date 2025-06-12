02cd213b8f3d6296f54c5e9439c921c8c4a7b8b6
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-12 11:43:35 +0200

    build the About page

---
+++ src/screens/About/index.tsx
@@ -0,0 +1,12 @@
+import Carousel from "../../components/carousel"
+
+function About() {
+  return (
+    <main className="">
+        <section className="h-[31rem]">
+            <Carousel/>
+        </section>
+    </main>
+  )
+}
+export default About
