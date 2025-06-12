ecf7e1f316baa107989e1e91aa08b7c4623c86c8
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-12 11:41:42 +0200

    rebuilding the cart logic for adding the product

--- src/context/cart/index.tsx
+++ src/context/cart/index.tsx
@@ -1,44 +1,50 @@
 import { create } from 'zustand';
+import { Product } from '../Product';
 
-export type Product = {
-  id?: number;
-  name: string;
-  price: number;
-  image: string;
-  quantity?: number;
-};
-
+type cart_element = {
+  include: boolean;
+} & Product;
 interface Cart {
-  list: Product[];
+  list: cart_element[];
   total_price: number;
-  add_Product: (item: Product) => void;
+  add_Product: (item: cart_element) => void;
   remove_Product: (id: number) => void;
-  update_Product: (item: Product) => void;
+  update_Product: (item: cart_element) => void;
 }
 
-let useCart = create<Cart>((set) => ({
+export let useCart = create<Cart>(set => ({
   list: [],
   total_price: 0,
-  add_Product: (item: Product) => {
-    item.price = parseFloat(item.price.toString());
-    item.image = item.image.toString();
-    set((state) => ({
-      list: [...state.list, item],
-      total_price: state.total_price + item.price * (item.quantity || 1),
-    }));
+  add_Product: (item: cart_element) => {
+    set(state => {
+      let filter = state.list.filter(iter => iter.id != item.id);
+      let element = state.list.find(ele => ele.id == item.id);
+      if (element) {
+        if (element.quantity) element.quantity += 1;
+        return {
+          list: [...filter, { ...element }],
+          total_price: state.total_price + item.price ,
+        };
+      } else {
+        return {
+          list: [...filter, { ...item, quantity: 1 }],
+          total_price: state.total_price + item.price ,
+        };
+      }
+    });
   },
   remove_Product: (id: number) =>
-    set((state) => {
-      let item = state.list.find((item) => item.id === id);
+    set(state => {
+      let item = state.list.find(item => item.id === id);
       return {
-        list: state.list.filter((item) => item.id !== id),
+        list: state.list.filter(item => item.id !== id),
         total_price:
           state.total_price - (item?.price || 0) * (item?.quantity || 1),
       };
     }),
-  update_Product: (item: Product) =>
-    set((state) => {
-      const index = state.list.findIndex((i) => i.id === item.id);
+  update_Product: item =>
+    set(state => {
+      const index = state.list.findIndex(i => i.id === item.id);
       if (index !== -1) {
         const updatedList = [...state.list];
         updatedList[index] = item;
