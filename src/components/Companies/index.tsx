48b2e3e81a2776b6bf9262c0173d2b41fe687565
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-13 05:57:56 +0200

    adding responser section

---
+++ src/components/Companies/index.tsx
@@ -0,0 +1,31 @@
+import {
+  TbBrandAdobe,
+  TbBrandAirbnb,
+  TbBrandLinkedin,
+  TbBrandMysql,
+} from 'react-icons/tb';
+
+let list = [
+  { name: 'Adobe', logo: <TbBrandAdobe size={"2rem"}/> },
+  { name: 'MySql', logo: <TbBrandMysql size={"2rem"} /> },
+  { name: 'Linkedin', logo: <TbBrandLinkedin size={"2rem"}/> },
+  { name: 'Airbnb', logo: <TbBrandAirbnb size={"2rem"}/> },
+];
+
+function Companies () {
+  return (
+    <section>
+      <div className='content three_cols_grid'>
+        {list.map(item => (
+          <div className='flex items-center justify-center h-[5rem]'>
+            <div className='flex gap-6'>
+              <div>{item.logo}</div>
+              <p>{item.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+export default Companies;
