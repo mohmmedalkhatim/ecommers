87ae1cda72e24a8ecd84088ab012094d0ebf2578
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-16 05:37:11 +0200

    adding collation section to show images

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,38 @@
 import Collation from '../../sections/Collation';
 import Hero from '../../sections/Hero';
-import Products from '../../sections/Products';
+import Products from '../Products';
 import Services from '../../sections/services/indxe';
 import Testimonies from '../../sections/Testimonies';
+import Carousel from '../../components/carousel';
+import Information from '../../sections/information';
+import List from '../../components/Product_list';
+import { useEffect, useState } from 'react';
+import { useProduct } from '../../context/Product';
+import Companies from '../../components/Companies';
 
-function Home() {
+let collation = {
+  main: '/imgs/ring.jpg',
+  images: [
+    '/imgs/ring.jpg',
+    '/imgs/ring.jpg',
+    '/imgs/ring.jpg',
+    '/imgs/ring.jpg',
+  ],
+};
+
+function Home () {
+  const [list,setList] = useState();
+  const fetchProducts = useProduct(state=>state.list)
+  useEffect(()=>{
+    fetchProducts(1,setList)
+  },[])
   return (
     <section>
       <Hero />
       <Testimonies />
-      <Services />
-      <Collation />
+      <Companies/>
+      {list && <List list={list}/>}
+      <Information/>
     </section>
   );
 }
