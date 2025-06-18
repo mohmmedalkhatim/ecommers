c11769572980f072090059464a6fd4bd08f1e9eb
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-18 09:23:45 +0200

    adding new title to the home screen

--- src/sections/Testimonies/index.tsx
+++ src/sections/Testimonies/index.tsx
@@ -23,8 +23,12 @@ let list = [
 
 function Testimonies() {
   return (
-    <section className="flex items-center justify-center  gap-4 content">
-      <div className="flex w-full gap-12 py-12 flex-col xl:flex-row">
+    <section className="flex flex-col  items-center justify-center  gap-4 content">
+      <div className='flex flex-col items-center  gap-12 py-12'>
+        <h3 className=''>People say to us</h3>
+        <div className='h-1 bg-blue-500 w-20 rounded-2xl'></div>
+      </div>
+      <div className="w-full gap-12 py-12 testimonies_container ">
         {list.map((item, i) => (
           <Commit key={i} {...item} />
         ))}
