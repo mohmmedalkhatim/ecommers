87e41ed1b66e9aa26685801d5568dae49bac937f
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-21 16:05:40 +0200

    rebuild the collation page

--- src/sections/Collation/index.tsx
+++ src/sections/Collation/index.tsx
@@ -1,15 +1,59 @@
-function Collation() {
+function Collation ({
+  main,
+  images,
+  rev,
+}: {
+  main: string;
+  images: string[];
+  rev: boolean;
+}) {
   return (
-    <section className="content py-12">
-      <div className="collation_container h-[30rem]">
-        <div className="element">hello</div>
-        <div className="collation_elements">
-          <div className="element">welcome</div>
-          <div className="element">welcome</div>
-          <div className="element">welcome</div>
-          <div className="element">welcome</div>
+    <section className='content py-12'>
+      {rev ? (
+        <div
+          className={`collation_container h-[30rem] `}
+          style={{ alignItems: 'revert' }}
+        >
+          <div className='element'>
+            <div
+              className={`w-full bg-cover h-full`}
+              style={{ backgroundImage: `url(${main})` }}
+            ></div>
+          </div>
+          <div className='collation_elements'>
+            {images.map((item, i) => (
+              <div className='element' key={i}>
+                <div
+                  className='w-full bg-cover h-full'
+                  style={{ backgroundImage: `url(${item})` }}
+                ></div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <div
+          className={`collation_container h-[30rem] `}
+          style={{ alignItems: 'revert' }}
+        >
+          <div className='collation_elements'>
+            {images.map((item, i) => (
+              <div className='element' key={i}>
+                <div
+                  className='w-full bg-cover h-full'
+                  style={{ backgroundImage: `url(${item})` }}
+                ></div>
+              </div>
+            ))}
+          </div>
+          <div className='element'>
+            <div
+              className={`w-full bg-cover h-full`}
+              style={{ backgroundImage: `url(${main})` }}
+            ></div>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
