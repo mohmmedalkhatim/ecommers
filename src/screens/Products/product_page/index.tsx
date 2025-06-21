b5cf7f598a4da86363b526c5bac6e9d55ffd4e49
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-21 16:03:21 +0200

    adding a carousel and adding a container to it

--- src/screens/Products/product_page/index.tsx
+++ src/screens/Products/product_page/index.tsx
@@ -3,20 +3,25 @@ import { useParams } from 'react-router-dom';
 import { Product, useProduct } from '../../../context/Product';
 import Product_info from './information';
 import Carousel from '../../../components/carousel';
+import { useLenis } from 'lenis/react';
 
 function ProductPage () {
   let { id } = useParams();
   let one = useProduct(state => state.product);
   let [product, setProduct] = useState({} as Product);
+
   useEffect(() => {
+    let lenis = document.querySelector('.lenis');
+    if (lenis) lenis.scrollTo(0, 0);
     if (id) one(id, setProduct);
   }, []);
-  console.log(product.pictures);
   if (product) {
     return (
-      <main >
-        <Product_info {...product}/>
-        <Carousel product={product} list={product.pictures}/>
+      <main>
+        <Product_info {...product} />
+        <section className='lg:px-20'>
+          <Carousel product={product} list={product.pictures} />
+        </section>
       </main>
     );
   }
