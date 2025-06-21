4169831c9265f59ed7b672c5c5e2cecf5ea42cb5
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-21 16:02:16 +0200

    adding links to the aside

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,20 +1,49 @@
-import { TbChevronsLeft } from 'react-icons/tb';
+import { TbBasket, TbUser, TbX } from 'react-icons/tb';
 import { useAside } from '../../context/Aside';
+import MyLink from '../Header/Link';
+import { Link } from 'react-router-dom';
 
-function Aside() {
-  let opened = useAside((state) => state.state);
-  let toggle = useAside((state) => state.toggle);
+let links = [
+  { name: 'Home', url: '/' },
+  { name: 'products', url: '/products' },
+  { name: 'collation', url: '/collations' },
+  { name: 'about', url: '/about' },
+];
+let info = [
+  { name: 'my_account', url: '/auth', icon: <TbUser size={'2rem'} /> },
+  { name: 'cart', url: '/cart', icon: <TbBasket size={'2rem'} /> },
+];
+
+function Aside () {
+  let opened = useAside(state => state.state);
+  let toggle = useAside(state => state.toggle);
   return (
     <aside
-      className="fixed top-0 w-[40rem] z-50 bg-white h-screen transition-all duration-300 border-l"
-      style={{ right: opened ? '0' : '-44rem' }}
+      className='fixed top-0 w-full md:w-screen z-50 bg-white h-screen transition-all duration-300 border-l'
+      style={{ right: opened ? '0' : '-100vw' }}
     >
-      <div className="w-full flex items-center justify-between p-12">
-        <div>aside</div>
-        <div onClick={toggle}>
-          <TbChevronsLeft size={'1.5rem'} />
-        </div>
+      <div className='absolute top-5 right-8' onClick={toggle}>
+        <TbX size={'1.5rem'} />
       </div>
+      <ul className='w-screen list-none flex flex-col pt-16'>
+        {links.map((item, i) => (
+            <Link
+              key={i}
+              to={item.url}
+              onClick={toggle}
+              onTouchEnd={toggle}
+              className='hover:bg-slate-200 px-4 py-6'
+              content={item.name}
+            >
+              {item.name}
+            </Link>
+        ))}
+      </ul>
+      <ul className='absolute bottom-8 flex gap-4 items-center text-[4rem] right-8'>
+        {info.map((item, i) => (
+          <MyLink key={i} url={item.url} content={item.icon} />
+        ))}
+      </ul>
     </aside>
   );
 }
