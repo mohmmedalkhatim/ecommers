ccd44960a941e9ed355f99d8290b6f03a4787c14
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-23 13:43:32 +0200

    adding logic for creating and getting the commits

--- src/context/commits/index.tsx
+++ src/context/commits/index.tsx
@@ -1,10 +1,58 @@
 import { create } from 'zustand';
+import { pb } from '../../main';
+import React, { Dispatch, SetStateAction } from 'react';
+import { Product } from '../Product';
+
+export type Commit = {
+  id: string;
+  user: string;
+  content: string;
+};
 
 interface commit_context {
-  one: () => {};
-  create: () => {};
-  modify: () => {};
-  
+  one: (
+    id: string,
+    setCommit: React.Dispatch<SetStateAction<Commit | undefined>>
+  ) => void;
+  create: (
+    content: string,
+    product_id: string,
+    commits: string[],
+    setProduct: Dispatch<SetStateAction<Product>>,
+    setCommits: Dispatch<SetStateAction<string[]>>
+  ) => void;
+  modify: () => void;
 }
 
-let useCommits = create();
+export let useCommits = create<commit_context>(() => ({
+  one: (id, setCommit) => {
+    pb.collection('commit')
+      .getOne<Commit>(id)
+      .then(res => {
+        console.log(res)
+        setCommit(res);
+      });
+  },
+  create: (
+    content: string,
+    product_id: string,
+    commits: string[],
+    setProduct,
+    setCommits
+  ) => {
+    pb.collection('commit')
+      .create({ content })
+      .then(res => {
+        pb.collection('product')
+          .update<Product>(product_id, {
+            product: pb.authStore.record?.name,
+            commits: [...commits, res.id],
+          })
+          .then(res => {
+            setProduct(res);
+          });
+        setCommits([...commits, res.id]);
+      });
+  },
+  modify: () => {},
+}));
