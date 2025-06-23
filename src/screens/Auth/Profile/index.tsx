9490a3e907b928c76a749ea6c7cb921f2d7eeaa0
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-06-23 13:46:32 +0200

    adding logic for refreshing the store when the user login

--- src/screens/Auth/Profile/index.tsx
+++ src/screens/Auth/Profile/index.tsx
@@ -2,15 +2,18 @@ import Button from '../../../components/Button';
 import { pb } from '../../../main';
 import { useAuth } from '../../../context/auth';
 import Avatar from './avatar';
+import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 
 function Profile () {
   let logout = useAuth(state => state.logout);
   let info = pb.authStore.record;
   let valid = pb.authStore.isValid;
+  let ref = useRef<HTMLAnchorElement>(null);
   if (valid || info) {
     return (
       <section className='h-[30rem] py-16'>
-        <div className='list-none flex justify-between  gap-8   h-[20rem] md:content'>
+        <div className='list-none flex justify-between  gap-8   h-[20rem]'>
           <div className=''>
             <div className='flex gap-8 p-4'>
               <Avatar />
@@ -21,7 +24,16 @@ function Profile () {
             </div>
           </div>
           <div className='flex flex-col justify-end'>
-            <Button onClick={logout}>logout</Button>
+            <Button
+              onClick={async () => {
+                await logout().then(() => {
+                  ref.current?.click();
+                });
+              }}
+            >
+              logout
+            </Button>
+            <Link ref={ref} to='/sign_in' style={{ display: 'none' }}></Link>
           </div>
         </div>
       </section>
