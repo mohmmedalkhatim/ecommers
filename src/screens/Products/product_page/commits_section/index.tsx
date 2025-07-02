325308435dc7ab5d2916e9e66d443b5300c570d4
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-07-02 12:48:16 +0200

    rebuild the layout for the commits section

--- src/screens/Products/product_page/commits_section/index.tsx
+++ src/screens/Products/product_page/commits_section/index.tsx
@@ -14,38 +14,41 @@ function Commits ({
   setProduct: Dispatch<SetStateAction<Product>>;
 }) {
   console.log(product.commits);
+  const [commits, setcommits] = useState(product.commits);
   const [content, setContent] = useState('');
   const create = useCommits(state => state.create);
   if (product.commits) {
     return (
-      <section className='content'>
-        <div className='content'>
-          {product.commits?.map(id => (
-            <Commit id={id} />
-          ))}
-        </div>
-        <div className='content'>
-          {pb.authStore.isValid ? (
-            <form
-              className='flex flex-col gap-4 content'
-              onSubmit={e => {
-                e.preventDefault();
-                create(content, product.id, commits, setProduct, setcommits);
-              }}
-            >
-              <Textarea
-                className={
-                  'border  h-[10rem] p-2 focus:ring-gray-900 rounded33 focus:outline-none'
-                }
-                onChange={e => setContent(e.target.value)}
-              ></Textarea>
-              <div className='flex justify-end items-end'>
-                <Button type='submit'>send</Button>
-              </div>
-            </form>
-          ) : (
-            <div>you have to sign in to add a commit</div>
-          )}
+      <section className='content '>
+        <div className='md:grid-cols-3 lg:px-12 grid'>
+          <div className='md:col-span-2'>
+            {product.commits?.map(id => (
+              <Commit id={id} />
+            ))}
+          </div>
+          <div className='col-span-1'>
+            {pb.authStore.isValid ? (
+              <form
+                className='flex flex-col gap-4'
+                onSubmit={e => {
+                  e.preventDefault();
+                  create(content, product.id, commits, setProduct, setcommits);
+                }}
+              >
+                <Textarea
+                  className={
+                    'border  h-[10rem] p-2 focus:ring-gray-900 rounded33 focus:outline-none'
+                  }
+                  onChange={e => setContent(e.target.value)}
+                ></Textarea>
+                <div className='flex justify-end items-end'>
+                  <Button type='submit'>send</Button>
+                </div>
+              </form>
+            ) : (
+              <div>you have to sign in to add a commit</div>
+            )}
+          </div>
         </div>
       </section>
     );
